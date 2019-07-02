caca6ba28e5967a841fb21aa00fcc08564bd6575
Matheus Sena <sena.matheus14@gmail.com>
2019-07-02 10:48:23 -0300

    fix __repr__ cell method

--- linked_list/cell.py
+++ linked_list/cell.py
@@ -6,7 +6,7 @@ class Cell:
 		self.previous = None
 
 	def __repr__(self):
-		return 'Cell(%s, %s, %s)' % (self.next, self.value, self.previous)
+		return 'Cell(%s, %s, %s)' % (self.previous, self.value, self.next)
 
 	def __str__(self):
 		return '%s' % self.value
