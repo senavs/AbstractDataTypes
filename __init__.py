9df6fb177a1c07105818dfa9cdbde6ac9cbb2325
Matheus Sena <sena.matheus14@gmail.com>
2019-07-02 16:43:47 -0300

    Add DoublyLinkedList and LinkedList

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 from .queue.queue import Queue
 from .deque.deque import Deque
 from .stack.stack import Stack
+from .linked_list.linked_list import LinkedList
+from .doubly_linked_list.doubly_linked_list import DoublyLinkedList
 from .binary_search_tree.tree import Tree
