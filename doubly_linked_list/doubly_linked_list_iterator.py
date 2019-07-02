48e56f6373007edc8724d5bca3fa2712222a870a
Matheus Sena <sena.matheus14@gmail.com>
2019-07-02 11:54:07 -0300

    Fix DoublyLinkedListIterator with self.size <= 1

--- doubly_linked_list/doubly_linked_list_iterator.py
+++ doubly_linked_list/doubly_linked_list_iterator.py
@@ -2,18 +2,23 @@ class DoublyLinkedListIterator:
 
 	def __init__(self, beginning):
 		self._current_cell = beginning
-		self._firts = True
+		self._first = True
 
 	def __iter__(self):
 		return self
 
 	def __next__(self):
-		if self._firts:
-			self._firts = False
+		try:
+			getattr(self._current_cell, "value")
+		except AttributeError:
+			raise StopIteration()
 		else:
-			try:
-				self._current_cell = self._current_cell.next
-				getattr(self._current_cell, "value")
-			except AttributeError:
+			if self._first:
+				self._first = False
+				return self._current_cell
+
+			self._current_cell  = self._current_cell.next
+			if self._current_cell is None:
 				raise StopIteration()
-		return self._current_cell.value
+			else:
+				return self._current_cell.value
