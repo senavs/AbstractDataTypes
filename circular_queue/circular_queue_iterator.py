952493b43a97284f61b523891820af80bf20e7b1
Matheus Sena <sena.matheus14@gmail.com>
2019-07-07 11:31:39 -0300

    Fixed ZeroDivisionError - circular_queue_iterator

--- circular_queue/circular_queue_iterator.py
+++ circular_queue/circular_queue_iterator.py
@@ -4,28 +4,27 @@ from .utils import next_position, prev_position
 class CircularQueueIterator:
 
 	def __init__(self, circular_queue):
-		self._queue = circular_queue
-		self._data = circular_queue._linear_data		
+		self._circular_queue = circular_queue
 		self._beginning = circular_queue._beginning
 		self._end = circular_queue._end
-		
-		self._current_index = circular_queue._beginning
+		self._current = circular_queue._beginning
 		self._first = True
 
 	def __iter__(self):
 		return self
 
 	def __next__(self):
-		if self._queue.empty():
+		if self._circular_queue.empty():
 			raise StopIteration()
 
 		if self._first:
 			self._first = False
-		elif self._current_index != self._end:
-			self._current_index = next_position(self._current_index, self._end)
-			if self._current_index == self._beginning:
+			return self._circular_queue._linear_data[self._current]
+		else:
+			self._current = next_position(self._current, self._circular_queue.max_size)
+			if self._current == self._beginning:
 				raise StopIteration()
-			elif self._data[self._current_index] is None:
+			elif self._circular_queue._linear_data[self._current] is None:
 				raise StopIteration()
-
-		return self._data[self._current_index]
+			else:
+				return self._circular_queue._linear_data[self._current]
